43b8e1906e98dce84cea709e5b570d90ab054694
AndresSArias <andres31321@gmail.com>
2021-12-15 14:14:01 -0500

    Primer paso para Abstract Factory

---
+++ Generador de personajes/src/Codigo/Armaduras/TIPOS/Yelmo_cuero.java
@@ -0,0 +1,49 @@
+package Codigo.Armaduras.TIPOS;
+
+import Codigo.Armaduras.Yelmo;
+
+public class Yelmo_cuero implements Yelmo {
+
+	@Override
+	public String getStats() {
+		return "Casco de baja protección creado con el cuero más fino del mundo";
+	}
+
+	@Override
+	public String getImage() {
+		return "/Material/yelmoCuero.png" ;
+	}
+
+	@Override
+	public int getCasco() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getGrebas() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getPeto() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getEscarpe() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getYelmo() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	
+}
